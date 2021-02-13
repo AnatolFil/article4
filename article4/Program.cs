a42534cc6af042ee4d86e54ce47293d8ef97ac00
AnatolFil <tolyanfilatov@mail.ru>
2021-02-13 18:50:22 +0700

    Добавьте файлы проекта.

---
+++ article4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace article4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class node
+    {
+        public string name;
+        public node[] children;
+    }
+    public class graph
+    {
+        public node[] nodes;
+    }
+}
